26e6876fd7b56a5a305045144bc4f9d5628a4410
SaniG <ssg97@hotmail.com>
2015-04-19 00:12:39 -0700

    Add account value request to save netliquidation to listbox first. Next will save into text file (csv)

--- frmMain.Designer.vb
+++ frmMain.Designer.vb
@@ -22,13 +22,32 @@ Partial Class frmMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.AxTws1 = New AxTWSLib.AxTws()
         Me.lbErrorAndLog = New System.Windows.Forms.ListBox()
         Me.lblConnected = New System.Windows.Forms.Label()
         Me.btnConnect = New System.Windows.Forms.Button()
         Me.btnDisconnect = New System.Windows.Forms.Button()
+        Me.chtEquity = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.lbData = New System.Windows.Forms.ListBox()
+        Me.dsAccountBalance = New System.Data.DataSet()
+        Me.AccountNetLiq = New System.Data.DataTable()
+        Me.DataColumn1 = New System.Data.DataColumn()
+        Me.DataColumn2 = New System.Data.DataColumn()
+        Me.DataColumn3 = New System.Data.DataColumn()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnStartAccSummary = New System.Windows.Forms.Button()
+        Me.tmrCollectAccSummary = New System.Windows.Forms.Timer(Me.components)
+        Me.btnAccUpdate = New System.Windows.Forms.Button()
         CType(Me.AxTws1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.chtEquity, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dsAccountBalance, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AccountNetLiq, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'AxTws1
@@ -76,11 +95,92 @@ Partial Class frmMain
         Me.btnDisconnect.Text = "Disconnect"
         Me.btnDisconnect.UseVisualStyleBackColor = True
         '
+        'chtEquity
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.chtEquity.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.chtEquity.Legends.Add(Legend2)
+        Me.chtEquity.Location = New System.Drawing.Point(12, 82)
+        Me.chtEquity.Name = "chtEquity"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.chtEquity.Series.Add(Series2)
+        Me.chtEquity.Size = New System.Drawing.Size(478, 354)
+        Me.chtEquity.TabIndex = 5
+        Me.chtEquity.Text = "Chart1"
+        '
+        'lbData
+        '
+        Me.lbData.FormattingEnabled = True
+        Me.lbData.Location = New System.Drawing.Point(516, 435)
+        Me.lbData.Name = "lbData"
+        Me.lbData.Size = New System.Drawing.Size(306, 121)
+        Me.lbData.TabIndex = 6
+        '
+        'dsAccountBalance
+        '
+        Me.dsAccountBalance.DataSetName = "NewDataSet"
+        Me.dsAccountBalance.Tables.AddRange(New System.Data.DataTable() {Me.AccountNetLiq})
+        '
+        'AccountNetLiq
+        '
+        Me.AccountNetLiq.Columns.AddRange(New System.Data.DataColumn() {Me.DataColumn1, Me.DataColumn2, Me.DataColumn3})
+        Me.AccountNetLiq.TableName = "AccountNetLiq"
+        '
+        'DataColumn1
+        '
+        Me.DataColumn1.ColumnName = "Date"
+        '
+        'DataColumn2
+        '
+        Me.DataColumn2.ColumnName = "Time"
+        '
+        'DataColumn3
+        '
+        Me.DataColumn3.ColumnName = "NetLiq"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(516, 59)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(306, 264)
+        Me.DataGridView1.TabIndex = 7
+        '
+        'btnStartAccSummary
+        '
+        Me.btnStartAccSummary.Location = New System.Drawing.Point(516, 361)
+        Me.btnStartAccSummary.Name = "btnStartAccSummary"
+        Me.btnStartAccSummary.Size = New System.Drawing.Size(122, 31)
+        Me.btnStartAccSummary.TabIndex = 8
+        Me.btnStartAccSummary.Text = "Start Acc Summary"
+        Me.btnStartAccSummary.UseVisualStyleBackColor = True
+        '
+        'tmrCollectAccSummary
+        '
+        Me.tmrCollectAccSummary.Interval = 5000
+        '
+        'btnAccUpdate
+        '
+        Me.btnAccUpdate.Location = New System.Drawing.Point(664, 363)
+        Me.btnAccUpdate.Name = "btnAccUpdate"
+        Me.btnAccUpdate.Size = New System.Drawing.Size(111, 29)
+        Me.btnAccUpdate.TabIndex = 9
+        Me.btnAccUpdate.Text = "Start Acc Update"
+        Me.btnAccUpdate.UseVisualStyleBackColor = True
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(731, 568)
+        Me.ClientSize = New System.Drawing.Size(861, 568)
+        Me.Controls.Add(Me.btnAccUpdate)
+        Me.Controls.Add(Me.btnStartAccSummary)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.lbData)
+        Me.Controls.Add(Me.chtEquity)
         Me.Controls.Add(Me.btnDisconnect)
         Me.Controls.Add(Me.btnConnect)
         Me.Controls.Add(Me.lblConnected)
@@ -89,6 +189,10 @@ Partial Class frmMain
         Me.Name = "frmMain"
         Me.Text = "IBac - IB Account Risk Management"
         CType(Me.AxTws1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.chtEquity, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dsAccountBalance, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AccountNetLiq, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -98,5 +202,16 @@ Partial Class frmMain
     Friend WithEvents lblConnected As System.Windows.Forms.Label
     Friend WithEvents btnConnect As System.Windows.Forms.Button
     Friend WithEvents btnDisconnect As System.Windows.Forms.Button
+    Friend WithEvents chtEquity As System.Windows.Forms.DataVisualization.Charting.Chart
+    Friend WithEvents lbData As System.Windows.Forms.ListBox
+    Friend WithEvents dsAccountBalance As System.Data.DataSet
+    Friend WithEvents AccountNetLiq As System.Data.DataTable
+    Friend WithEvents DataColumn1 As System.Data.DataColumn
+    Friend WithEvents DataColumn2 As System.Data.DataColumn
+    Friend WithEvents DataColumn3 As System.Data.DataColumn
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents btnStartAccSummary As System.Windows.Forms.Button
+    Friend WithEvents tmrCollectAccSummary As System.Windows.Forms.Timer
+    Friend WithEvents btnAccUpdate As System.Windows.Forms.Button
 
 End Class
--- frmMain.vb
+++ frmMain.vb
@@ -1,8 +1,10 @@
 ﻿Imports System.IO
+Imports System.Timers
 
 Public Class frmMain
     Public g_accounts As String() = {"U129661", "U1465027"}
     Dim g_connecting_inprogress As Boolean = False
+    Dim g_onetime_call_account As Boolean = False
 
     Dim IBCONNECTION_NUMBER As Integer = 69 ' Must be unique number or IB will not connect us
 
@@ -46,5 +48,110 @@ Public Class frmMain
         Call AxTws1.disconnect()
         lblConnected.Text = "DISCONNECTED"
         lblConnected.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnStartAccSummary_Click(sender As Object, e As EventArgs) Handles btnStartAccSummary.Click
+
+
+        If String.Compare(lblConnected.Text, "CONNECTED") = 0 Then  '0 means equal
+            tmrCollectAccSummary.Start()
+        Else
+            MsgBox("Make sure you are connected to TWS")
+        End If
+
+
+
+    End Sub
+
+    Private Sub tmrCollectAccSummary_Tick(sender As Object, e As EventArgs) Handles tmrCollectAccSummary.Tick
+        'Every 5 secs this fires
+        'If market hours, then divide minute % 5. if mod is = 0, it is 5th minute (to collect 5 minutes data)
+        ' Mark date/time, acc value
+        ' Write to file
+
+        Dim market_starttime As DateTime = #12:00:00 AM#
+        Dim market_endtime As DateTime = #1:00:00 PM#
+
+        'Console.WriteLine(TimeOfDay)
+        'Test if it is market hours.
+        If TimeOfDay > market_starttime Then
+            If (TimeOfDay.Minute Mod 5) = 0 And Not g_onetime_call_account Then
+                g_onetime_call_account = True
+                Console.WriteLine(TimeOfDay & " write file")
+                Call AxTws1.reqAccountSummary(1, "All", "AccruedCash,BuyingPower,NetLiquidation")
+
+            End If
+
+        End If
+
+
+    End Sub
+
+    Private Sub AxTws1_accountSummary(sender As Object, e As AxTWSLib._DTwsEvents_accountSummaryEvent) Handles AxTws1.accountSummary
+
+        Dim filename As String
+
+
+        If e.account = g_accounts(0) Then
+
+            filename = "c:\" & g_accounts(0) & "acc_equity_5min.csv"
+            If e.tag = "NetLiquidation" Then
+                Using accFile As StreamWriter = New StreamWriter(filename, True)
+                    accFile.WriteLine(Now.ToString & "," & e.value)
+                End Using
+                lbData.Items.Add(g_accounts(0) & "," & e.value)
+                lbData.TopIndex = lbData.Items.Count - 1
+
+            End If
+        End If
+
+
+        If e.account = g_accounts(1) Then
+            filename = "c:\" & g_accounts(1) & "acc_equity_5min.csv"
+            If e.tag = "NetLiquidation" Then
+                Using accFile As StreamWriter = New StreamWriter(filename, True)
+                    accFile.WriteLine(Now.ToString & "," & e.value)
+                End Using
+                lbData.Items.Add(g_accounts(1) & "," & e.value)
+                lbData.TopIndex = lbData.Items.Count - 1
+
+            End If
+        End If
+
+
+        'Call AxTws1.cancelAccountSummary(1)
+
+    End Sub
+
+    Private Sub AxTws1_accountSummaryEnd(sender As Object, e As AxTWSLib._DTwsEvents_accountSummaryEndEvent) Handles AxTws1.accountSummaryEnd
+        lbData.Items.Add("accountSummaryEnd")
+        lbData.TopIndex = lbData.Items.Count - 1
+        Call AxTws1.cancelAccountSummary(1)
+    End Sub
+
+    Private Sub btnAccUpdate_Click(sender As Object, e As EventArgs) Handles btnAccUpdate.Click
+        AxTws1.reqAccountUpdates(1, g_accounts(0))  ' 1 = start receiving, 0 stop
+        ' AxTws1.reqAccountUpdates(1, g_accounts(1))
+
+    End Sub
+
+    Private Sub AxTws1_updateAccountValue(sender As Object, e As AxTWSLib._DTwsEvents_updateAccountValueEvent) Handles AxTws1.updateAccountValue
+
+        For i As Integer = 0 To g_accounts.Length - 1
+
+            If e.accountName = g_accounts(i) And e.key = "NetLiquidation" Then
+                AxTws1.reqAccountUpdates(0, g_accounts(i))  ' 1 = start receiving, 0 stop
+
+                ' Make sure it is not end of array and access out-of-bound g_accounts array element at i+1
+                If i < g_accounts.Length - 1 Then
+                    AxTws1.reqAccountUpdates(1, g_accounts(i + 1))
+                End If
+
+                lbData.Items.Add(e.accountName & "," & TimeOfDay & "," & e.value)
+                lbData.TopIndex = lbData.Items.Count - 1
+            End If
+
+        Next
+
     End Sub
 End Class
