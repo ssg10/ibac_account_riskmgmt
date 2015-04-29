e2e23a4b487aee1ac798684deab157a31918f2ba
SaniG <ssg97@hotmail.com>
2015-04-29 01:32:15 -0700

    Add account summary with timed update on chart and files

--- frmMain.Designer.vb
+++ frmMain.Designer.vb
@@ -24,9 +24,9 @@ Partial Class frmMain
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
-        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
-        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
-        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.AxTws1 = New AxTWSLib.AxTws()
         Me.lbErrorAndLog = New System.Windows.Forms.ListBox()
         Me.lblConnected = New System.Windows.Forms.Label()
@@ -43,6 +43,11 @@ Partial Class frmMain
         Me.btnStartAccSummary = New System.Windows.Forms.Button()
         Me.tmrCollectAccSummary = New System.Windows.Forms.Timer(Me.components)
         Me.btnAccUpdate = New System.Windows.Forms.Button()
+        Me.tmrAccUpdate = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.cbDaysinchart = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.AxTws1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.chtEquity, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.dsAccountBalance, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -53,7 +58,7 @@ Partial Class frmMain
         'AxTws1
         '
         Me.AxTws1.Enabled = True
-        Me.AxTws1.Location = New System.Drawing.Point(601, 472)
+        Me.AxTws1.Location = New System.Drawing.Point(728, 527)
         Me.AxTws1.Name = "AxTws1"
         Me.AxTws1.OcxState = CType(resources.GetObject("AxTws1.OcxState"), System.Windows.Forms.AxHost.State)
         Me.AxTws1.Size = New System.Drawing.Size(100, 50)
@@ -62,16 +67,17 @@ Partial Class frmMain
         'lbErrorAndLog
         '
         Me.lbErrorAndLog.FormattingEnabled = True
+        Me.lbErrorAndLog.HorizontalScrollbar = True
         Me.lbErrorAndLog.Location = New System.Drawing.Point(12, 461)
         Me.lbErrorAndLog.Name = "lbErrorAndLog"
-        Me.lbErrorAndLog.Size = New System.Drawing.Size(478, 95)
+        Me.lbErrorAndLog.Size = New System.Drawing.Size(422, 95)
         Me.lbErrorAndLog.TabIndex = 1
         '
         'lblConnected
         '
         Me.lblConnected.AutoSize = True
         Me.lblConnected.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblConnected.Location = New System.Drawing.Point(512, 18)
+        Me.lblConnected.Location = New System.Drawing.Point(404, 18)
         Me.lblConnected.Name = "lblConnected"
         Me.lblConnected.Size = New System.Drawing.Size(189, 24)
         Me.lblConnected.TabIndex = 2
@@ -97,26 +103,27 @@ Partial Class frmMain
         '
         'chtEquity
         '
-        ChartArea2.Name = "ChartArea1"
-        Me.chtEquity.ChartAreas.Add(ChartArea2)
-        Legend2.Name = "Legend1"
-        Me.chtEquity.Legends.Add(Legend2)
-        Me.chtEquity.Location = New System.Drawing.Point(12, 82)
+        ChartArea1.Name = "ChartArea1"
+        Me.chtEquity.ChartAreas.Add(ChartArea1)
+        Legend1.Enabled = False
+        Legend1.Name = "Legend1"
+        Me.chtEquity.Legends.Add(Legend1)
+        Me.chtEquity.Location = New System.Drawing.Point(12, 89)
         Me.chtEquity.Name = "chtEquity"
-        Series2.ChartArea = "ChartArea1"
-        Series2.Legend = "Legend1"
-        Series2.Name = "Series1"
-        Me.chtEquity.Series.Add(Series2)
-        Me.chtEquity.Size = New System.Drawing.Size(478, 354)
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.chtEquity.Series.Add(Series1)
+        Me.chtEquity.Size = New System.Drawing.Size(719, 354)
         Me.chtEquity.TabIndex = 5
         Me.chtEquity.Text = "Chart1"
         '
         'lbData
         '
         Me.lbData.FormattingEnabled = True
-        Me.lbData.Location = New System.Drawing.Point(516, 435)
+        Me.lbData.Location = New System.Drawing.Point(448, 461)
         Me.lbData.Name = "lbData"
-        Me.lbData.Size = New System.Drawing.Size(306, 121)
+        Me.lbData.Size = New System.Drawing.Size(258, 95)
         Me.lbData.TabIndex = 6
         '
         'dsAccountBalance
@@ -144,14 +151,14 @@ Partial Class frmMain
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(516, 59)
+        Me.DataGridView1.Location = New System.Drawing.Point(770, 12)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.Size = New System.Drawing.Size(306, 264)
         Me.DataGridView1.TabIndex = 7
         '
         'btnStartAccSummary
         '
-        Me.btnStartAccSummary.Location = New System.Drawing.Point(516, 361)
+        Me.btnStartAccSummary.Location = New System.Drawing.Point(782, 294)
         Me.btnStartAccSummary.Name = "btnStartAccSummary"
         Me.btnStartAccSummary.Size = New System.Drawing.Size(122, 31)
         Me.btnStartAccSummary.TabIndex = 8
@@ -164,18 +171,63 @@ Partial Class frmMain
         '
         'btnAccUpdate
         '
-        Me.btnAccUpdate.Location = New System.Drawing.Point(664, 363)
+        Me.btnAccUpdate.Location = New System.Drawing.Point(965, 527)
         Me.btnAccUpdate.Name = "btnAccUpdate"
         Me.btnAccUpdate.Size = New System.Drawing.Size(111, 29)
         Me.btnAccUpdate.TabIndex = 9
         Me.btnAccUpdate.Text = "Start Acc Update"
         Me.btnAccUpdate.UseVisualStyleBackColor = True
         '
+        'tmrAccUpdate
+        '
+        Me.tmrAccUpdate.Interval = 5000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(990, 302)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'cbDaysinchart
+        '
+        Me.cbDaysinchart.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cbDaysinchart.FormattingEnabled = True
+        Me.cbDaysinchart.Items.AddRange(New Object() {"1", "2", "3", "4", "5"})
+        Me.cbDaysinchart.Location = New System.Drawing.Point(782, 366)
+        Me.cbDaysinchart.Name = "cbDaysinchart"
+        Me.cbDaysinchart.Size = New System.Drawing.Size(121, 21)
+        Me.cbDaysinchart.TabIndex = 11
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(779, 344)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "Days in Chart"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(990, 334)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(861, 568)
+        Me.ClientSize = New System.Drawing.Size(1088, 568)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cbDaysinchart)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnAccUpdate)
         Me.Controls.Add(Me.btnStartAccSummary)
         Me.Controls.Add(Me.DataGridView1)
@@ -187,6 +239,7 @@ Partial Class frmMain
         Me.Controls.Add(Me.lbErrorAndLog)
         Me.Controls.Add(Me.AxTws1)
         Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "IBac - IB Account Risk Management"
         CType(Me.AxTws1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.chtEquity, System.ComponentModel.ISupportInitialize).EndInit()
@@ -213,5 +266,10 @@ Partial Class frmMain
     Friend WithEvents btnStartAccSummary As System.Windows.Forms.Button
     Friend WithEvents tmrCollectAccSummary As System.Windows.Forms.Timer
     Friend WithEvents btnAccUpdate As System.Windows.Forms.Button
+    Friend WithEvents tmrAccUpdate As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents cbDaysinchart As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
 
 End Class
--- frmMain.vb
+++ frmMain.vb
@@ -5,14 +5,23 @@ Public Class frmMain
     Public g_accounts As String() = {"U129661", "U1465027"}
     Dim g_connecting_inprogress As Boolean = False
     Dim g_onetime_call_account As Boolean = False
+    Dim g_is_acc_summary_subscribed As Boolean = False
+    Dim g_sec_when_data_collected As Integer = 59
+    Dim g_min_when_data_collected As Integer = 59
+    Dim g_is_accsummary_api_called As Boolean = False
+    Dim g_dump_daily_data_once As Boolean = False
+    Dim g_write_once_per_run As Boolean = True
+    'Dim MARKET_CLOSE_TIME As DateTime = #1:00:00 PM#
+    'Dim MARKET_OPEN_TIME As DateTime = #6:30:00 AM#
+
+    'For test
+    Dim MARKET_CLOSE_TIME As DateTime = #2:00:00 AM#
+    Dim MARKET_OPEN_TIME As DateTime = #12:30:00 AM#
 
     Dim IBCONNECTION_NUMBER As Integer = 69 ' Must be unique number or IB will not connect us
+    Dim IB_ACC_SUMMARY_REQID As Integer = 1
 
-
-
-    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+    Dim dtAccEquity As New DataTable("dtAccEquity")
 
     Private Sub AxTws1_errMsg(sender As Object, e As AxTWSLib._DTwsEvents_errMsgEvent) Handles AxTws1.errMsg
         lbErrorAndLog.Items.Add(Now.ToString & ":  " & e.errorMsg)
@@ -52,14 +61,22 @@ Public Class frmMain
 
     Private Sub btnStartAccSummary_Click(sender As Object, e As EventArgs) Handles btnStartAccSummary.Click
 
-
         If String.Compare(lblConnected.Text, "CONNECTED") = 0 Then  '0 means equal
-            tmrCollectAccSummary.Start()
+            If Not g_is_acc_summary_subscribed Then
+                tmrCollectAccSummary.Start()
+                'Call AxTws1.reqAccountSummary(IB_ACC_SUMMARY_REQID, "All", "AccruedCash,BuyingPower,NetLiquidation")
+                g_is_acc_summary_subscribed = True
+                btnStartAccSummary.Text = "Stop Acc Summary"
+            Else
+                tmrCollectAccSummary.Stop()
+                AxTws1.cancelAccountSummary(IB_ACC_SUMMARY_REQID)
+                g_is_acc_summary_subscribed = False
+                btnStartAccSummary.Text = "Start Acc Summary"
+            End If
+
         Else
             MsgBox("Make sure you are connected to TWS")
         End If
-
-
 
     End Sub
 
@@ -69,57 +86,160 @@ Public Class frmMain
         ' Mark date/time, acc value
         ' Write to file
 
-        Dim market_starttime As DateTime = #12:00:00 AM#
-        Dim market_endtime As DateTime = #1:00:00 PM#
+        '----- Start collecting data during market hours.
+        If (TimeOfDay >= MARKET_OPEN_TIME) And (TimeOfDay <= MARKET_CLOSE_TIME) Then
 
-        'Console.WriteLine(TimeOfDay)
-        'Test if it is market hours.
-        If TimeOfDay > market_starttime Then
-            If (TimeOfDay.Minute Mod 5) = 0 And Not g_onetime_call_account Then
-                g_onetime_call_account = True
-                Console.WriteLine(TimeOfDay & " write file")
-                Call AxTws1.reqAccountSummary(1, "All", "AccruedCash,BuyingPower,NetLiquidation")
+            ' If we are at the next min, reset the flags. Purpose of flags is to write the data once in i-th minute, since timer runs more often
+            ' =0 because when current min is 0 = 59+1 will be wrong. (when min rolls back)
+            'If (TimeOfDay.Minute = (g_min_when_data_collected + 1)) Or (TimeOfDay.Minute = 0) Then
+            If (TimeOfDay.Minute <> g_min_when_data_collected) Then
+                Console.WriteLine(Now.ToString & " Timeofday.minute <> " & g_min_when_data_collected & " " & g_sec_when_data_collected)
+                g_sec_when_data_collected = 59
+                g_min_when_data_collected = 59
+                g_write_once_per_run = True
+            End If
+
+            ' See if it is (mod) ith minute to grab i min account data
+            If ((TimeOfDay.Minute Mod 1) = 0) And (TimeOfDay.Second < g_sec_when_data_collected) Then
+                Console.WriteLine(TimeOfDay & " " & TimeOfDay.Second & " " & g_sec_when_data_collected & " write file")
+                'Save second data to be compared - used to avoid getting too many reading in 5 min
+                g_sec_when_data_collected = TimeOfDay.Second
+                g_min_when_data_collected = TimeOfDay.Minute
+
+
+                Call AxTws1.reqAccountSummary(IB_ACC_SUMMARY_REQID, "All", "NetLiquidation")
+                g_is_accsummary_api_called = True
+
+            Else
+                Console.WriteLine(TimeOfDay & " " & g_min_when_data_collected & " " & g_sec_when_data_collected & " Else if not mod 1")
+                ' add flag so that it is called only if api has been called once
+                If g_is_accsummary_api_called Then
+                    Console.WriteLine(TimeOfDay & "Call cancelAccountSummary")
+                    Call AxTws1.cancelAccountSummary(IB_ACC_SUMMARY_REQID)
+                    g_is_accsummary_api_called = False
+                End If
 
             End If
 
+        Else
+            ' make sure all flags reset
+            Console.WriteLine("Reset flags")
+            g_sec_when_data_collected = 59
+            g_min_when_data_collected = 59
         End If
 
+        '----- If now is after market hours by a bit, then we collect the end of day account data
+        If (TimeOfDay > MARKET_CLOSE_TIME) And (TimeOfDay < MARKET_CLOSE_TIME.AddMinutes(3)) And Not g_dump_daily_data_once Then
+
+            Console.WriteLine(Now & " Write EOD data")
+
+            Call AxTws1.reqAccountSummary(IB_ACC_SUMMARY_REQID, "All", "NetLiquidation")  'assume data req comes back faster than timer fires (1 sec or so)
+
+        ElseIf (TimeOfDay > MARKET_CLOSE_TIME.AddMinutes(15)) And (TimeOfDay < MARKET_CLOSE_TIME.AddMinutes(30)) Then
+
+            ' Reset the flag so that next day's closing price can be recorded again (anytime between 1:00pm to 1:15pm, hit once using flag)
+            g_dump_daily_data_once = False
+
+        End If
 
     End Sub
 
     Private Sub AxTws1_accountSummary(sender As Object, e As AxTWSLib._DTwsEvents_accountSummaryEvent) Handles AxTws1.accountSummary
 
         Dim filename As String
+        Try
+            If (TimeOfDay >= MARKET_OPEN_TIME) And (TimeOfDay <= MARKET_CLOSE_TIME) Then
+
+                Console.WriteLine(Now & " " & e.account)
+
+                If g_write_once_per_run Then
+
+                    If e.account = g_accounts(0) Then
+
+                        filename = "c:\" & g_accounts(0) & "acc_equity_5min.csv"
+                        If e.tag = "NetLiquidation" Then
+                            Using accFile As StreamWriter = New StreamWriter(filename, True)
+                                accFile.WriteLine(Now.ToString & "," & e.value)
+                            End Using
+                            lbData.Items.Add(Now.ToString & "," & g_accounts(0) & "," & e.value)
+                            lbData.TopIndex = lbData.Items.Count - 1
+
+                            dtAccEquity.Rows.Add({Now.ToString, e.value})  ' Parms take array
+                            DataGridView1.DataSource = dtAccEquity.DefaultView
+
+                            chtEquity.Series(0).Points.Clear()
+
+                            chtEquity.DataSource = ""
+
+                            chtEquity.ResetAutoValues()
+                            chtEquity.DataSource = dtAccEquity
+                            chtEquity.DataBind()
+
+                            With chtEquity.Series(0)
+                                .Points.Clear()
+
+                                .Points.DataBind(dtAccEquity.DefaultView, "DateTime", "NetLiquidity", Nothing)
+                                '.XValueMember = "<DateTime>"
+                                '.YValueMembers = "<NetLiquidity>"
+                                .ChartType = DataVisualization.Charting.SeriesChartType.Line
+                            End With
+
+                            chtEquity.ResetAutoValues()
+
+                        End If
+                    End If
+
+                    If e.account = g_accounts(1) Then
+                        filename = "c:\" & g_accounts(1) & "acc_equity_5min.csv"
+                        If e.tag = "NetLiquidation" Then
+                            Using accFile As StreamWriter = New StreamWriter(filename, True)
+                                accFile.WriteLine(Now.ToString & "," & e.value)
+                            End Using
+                            lbData.Items.Add(TimeOfDay.ToString & "," & g_accounts(1) & "," & e.value)
+                            lbData.TopIndex = lbData.Items.Count - 1
+                            g_write_once_per_run = False
+                        End If
+                    End If
+                End If
 
 
-        If e.account = g_accounts(0) Then
+            End If '==== If TimeOfDay > MARKET_OPEN_TIME Then
 
-            filename = "c:\" & g_accounts(0) & "acc_equity_5min.csv"
-            If e.tag = "NetLiquidation" Then
-                Using accFile As StreamWriter = New StreamWriter(filename, True)
-                    accFile.WriteLine(Now.ToString & "," & e.value)
-                End Using
-                lbData.Items.Add(g_accounts(0) & "," & e.value)
-                lbData.TopIndex = lbData.Items.Count - 1
+            '----- If now is after market hours by a bit, then we collect the end of day account data
+            If (TimeOfDay > MARKET_CLOSE_TIME) And (TimeOfDay < MARKET_CLOSE_TIME.AddMinutes(3)) Then
+
+                If e.account = g_accounts(0) Then
+                    ' Write file for first account
+                    filename = "c:\" & g_accounts(0) & "acc_equity_daily.csv"
+                    Using accFile As StreamWriter = New StreamWriter(filename, True)
+                        accFile.WriteLine(Now.ToString & "," & e.value)
+                    End Using
+                    lbData.Items.Add(g_accounts(0) & "," & e.value)
+                    lbData.TopIndex = lbData.Items.Count - 1
+                End If
+
+                If e.account = g_accounts(1) Then
+                    ' Write file for second account
+                    filename = "c:\" & g_accounts(1) & "acc_equity_daily.csv"
+                    Using accFile As StreamWriter = New StreamWriter(filename, True)
+                        accFile.WriteLine(Now.ToString & "," & e.value)
+                    End Using
+                    lbData.Items.Add(g_accounts(1) & "," & e.value)
+                    lbData.TopIndex = lbData.Items.Count - 1
+                End If
+
+                Console.WriteLine(Now.ToString & " " & g_dump_daily_data_once & "  Market close and collect data")
+                ' set flag back so that timer next fire wont call reqAccountSummary again
+                g_dump_daily_data_once = True
+
+                Call AxTws1.cancelAccountSummary(IB_ACC_SUMMARY_REQID)
 
             End If
-        End If
 
+        Catch ex As Exception
 
-        If e.account = g_accounts(1) Then
-            filename = "c:\" & g_accounts(1) & "acc_equity_5min.csv"
-            If e.tag = "NetLiquidation" Then
-                Using accFile As StreamWriter = New StreamWriter(filename, True)
-                    accFile.WriteLine(Now.ToString & "," & e.value)
-                End Using
-                lbData.Items.Add(g_accounts(1) & "," & e.value)
-                lbData.TopIndex = lbData.Items.Count - 1
-
-            End If
-        End If
-
-
-        'Call AxTws1.cancelAccountSummary(1)
+            MessageBox.Show(ex.ToString)
+        End Try
 
     End Sub
 
@@ -130,6 +250,9 @@ Public Class frmMain
     End Sub
 
     Private Sub btnAccUpdate_Click(sender As Object, e As EventArgs) Handles btnAccUpdate.Click
+        ' Account update won't work if ninjatrader is connected
+        ' Therefore, we are using accountsummary for the meantime, but if TWS is not at second account, then it will return only the main account.
+        '
         AxTws1.reqAccountUpdates(1, g_accounts(0))  ' 1 = start receiving, 0 stop
         ' AxTws1.reqAccountUpdates(1, g_accounts(1))
 
@@ -152,6 +275,113 @@ Public Class frmMain
             End If
 
         Next
+
+    End Sub
+
+    Private Sub tmrAccUpdate_Tick(sender As Object, e As EventArgs) Handles tmrAccUpdate.Tick
+        'Every 5 secs this fires
+        'If market hours, then divide minute % 5. if mod is = 0, it is 5th minute (to collect 5 minutes data)
+        ' Mark date/time, acc value
+        ' Write to file
+
+        ' The issue is accountvalue can 
+
+        Dim market_starttime As DateTime = #12:00:00 AM#
+        Dim market_endtime As DateTime = #1:00:00 PM#
+
+        'Console.WriteLine(TimeOfDay)
+        'Test if it is market hours.
+        If TimeOfDay > market_starttime Then
+            If (TimeOfDay.Minute Mod 5) = 0 Then
+
+                ' need this to be called once only
+                Console.WriteLine(TimeOfDay & " write file")
+
+                AxTws1.reqAccountUpdates(1, g_accounts(0))
+
+
+
+            End If
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim file As String = "c:\" & g_accounts(0) & "acc_equity_5min.csv"
+
+        Try 'file might throw exception if not exist
+
+
+            chtEquity.Series(0).Points.Clear()
+
+            chtEquity.DataSource = ""
+
+            dtAccEquity.Clear()
+
+            Using sr As StreamReader = New StreamReader(file)
+
+                While Not sr.EndOfStream
+                    Dim dataSplits As String() = sr.ReadLine.Split(",")
+                    dtAccEquity.Rows.Add(dataSplits)
+                End While
+            End Using
+
+            DataGridView1.DataSource = dtAccEquity.DefaultView
+
+
+            chtEquity.DataSource = dtAccEquity
+            chtEquity.DataBind()
+
+            With chtEquity.Series(0)
+                .Points.Clear()
+
+                .Points.DataBind(dtAccEquity.DefaultView, "DateTime", "NetLiquidity", Nothing)
+                '.XValueMember = "<DateTime>"
+                '.YValueMembers = "<NetLiquidity>"
+                .ChartType = DataVisualization.Charting.SeriesChartType.Line
+            End With
+
+            chtEquity.ResetAutoValues()
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString)
+
+        End Try
+
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'Init datagridview control
+        dtAccEquity.Columns.Add("DateTime")
+        dtAccEquity.Columns.Add("NetLiquidity")
+
+        chtEquity.Series.Clear()
+
+        'Create chart
+        chtEquity.Series.Add("AccEquity")
+        chtEquity.Name = "AccEquity"
+
+        With chtEquity.ChartAreas(0)
+            .AxisX.Title = "Date"
+            .AxisY.Title = "Units"
+            .AxisY.Minimum = 8000
+            .AxisY.Maximum = 15000
+        End With
+
+        cbDaysinchart.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub AxTws1_accountDownloadEnd(sender As Object, e As AxTWSLib._DTwsEvents_accountDownloadEndEvent) Handles AxTws1.accountDownloadEnd
+        Console.WriteLine("accountDownlowdEnd")
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        cbDaysinchart.SelectedItem = 0
 
     End Sub
 End Class
